64b5801daf2520d991e1ac9f40e56820d687986e
Victor Lin <linvictorw@gmail.com>
2021-02-27 18:30:35 +0800

    Updates

---
+++ precache-manifest.1c363cb6cce9ef5af10864669c5b9aed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de51e7c51b80aaf32be137b30b00c89",
+    "url": "/hooks-simple/index.html"
+  },
+  {
+    "revision": "33c26b7901b00d8b30e0",
+    "url": "/hooks-simple/static/js/2.3c08d5cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hooks-simple/static/js/2.3c08d5cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e67e8d67ea8e8de66ac",
+    "url": "/hooks-simple/static/js/main.cbe73132.chunk.js"
+  },
+  {
+    "revision": "b98084898c86adbc2d27",
+    "url": "/hooks-simple/static/js/runtime-main.4e6674c9.js"
+  }
+]);
